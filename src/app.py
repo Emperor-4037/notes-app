0ec9edfe704948ef90742de969aaf818a7ec33d5
Emperor-4037 <vasudeva4037@gmail.com>
2026-03-23 17:48:32 +0000

    Cors

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 
 from src.database.session import engine
@@ -21,6 +22,14 @@ app = FastAPI(
     title="Notes app",
     version="1.0.0",
     lifespan=lifespan
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origin_regex=".*",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(auth_router)
